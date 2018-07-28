438b52500b7114dfffe4ae125ebf0515031f131f
Huỳnh Quang Thái <thai.huynh8324@gmail.com>
2018-07-28 12:12:11 +0700

    troll

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 
+import './styles/common.style.scss';
+
 export default class App extends Component {
 	render() {
-		return <h1>Hello World!</h1>;
+		return (
+			<div className='bg'>
+				<div className='hero-text'>
+					<h1 style={{ fontSize: 50 }}>Weekend!!! :))</h1>
+					<p>I'm a Developer</p>
+					<button>Hire me</button>
+				</div>
+			</div>
+		);
 	}
 }
